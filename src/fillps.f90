49f3f7cfa1ad94932efac9ed7efbd3e5bf24fa24
maxcuda <mfatica@gmail.com>
2018-12-28 12:24:37 -0800

    In fillps, dtidzfi is computed on GPU

--- src/fillps.f90
+++ src/fillps.f90
@@ -24,7 +24,7 @@ module mod_fillps
     real(8) :: dtidxi,dtidyi!,dtidzi
     real(8), dimension(0:n(3)+1) :: dtidzfi
 #ifdef USE_CUDA
-    attributes(managed):: up,vp,wp,p,dtidzfi
+    attributes(managed):: up,vp,wp,p,dtidzfi,dzfi
     integer:: istat
 #endif
     integer :: i,j,k,im,jm,km
@@ -32,26 +32,24 @@ module mod_fillps
     dtidxi = dti*dli(1)
     dtidyi = dti*dli(2)
     !dtidzi = dti*dli(3)
-    dtidzfi(:) = dti*dzfi(:)
 #ifdef USE_CUDA
-    !$cuf kernel do(3) <<<*,*>>>
-    do k=1,n(3)
-      do j=1,n(2)
-        do i=1,n(1)
-          km = k-1
-          jm = j-1
-          im = i-1
+    !$cuf kernel do(1) <<<*,*>>>
+    do k=0,n(3)+1
+     dtidzfi(k) = dti*dzfi(k)
+    end do
+   !$cuf kernel do(3) <<<*,*>>>
 #else
+    dtidzfi(:) = dti*dzfi(:)
     !$OMP PARALLEL DO DEFAULT(none) &
     !$OMP SHARED(n,p,up,vp,wp,dtidzfi,dtidyi,dtidxi) &
     !$OMP PRIVATE(i,j,k,im,jm,km)
+#endif
     do k=1,n(3)
       km = k-1
       do j=1,n(2)
         jm = j-1
         do i=1,n(1)
           im = i-1
-#endif
           p(i,j,k) = ( &
                       (wp(i,j,k)-wp(i,j,km))*dtidzfi(k)+ &
                       (vp(i,j,k)-vp(i,jm,k))*dtidyi    + &
